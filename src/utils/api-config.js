262da237b379f86d6f191de75a7a7bf77851dc6e
narendra lohar <92054294+codernarendra@users.noreply.github.com>
2025-05-07 14:53:52 +0530

    Update api-config.js

--- src/utils/api-config.js
+++ src/utils/api-config.js
@@ -2,8 +2,8 @@ const API_BASE_URL = "http://127.0.0.1:9000/api/v2";
 export const IMAGE_URL = "http://localhost:9000";
 export const RAZORPAY_KEY_ID = "rzp_test_sbbCHuQzenmT45";
 
-// const API_BASE_URL = "https://admissions-direct.in/graceofcows-backend/api/v2";
-// export const IMAGE_URL = "https://admissions-direct.in/graceofcows-backend/storage";
+// const API_BASE_URL = "http://3.110.247.112/zozo_cart_api/api/v2";
+// export const IMAGE_URL = "http://3.110.247.112/zozo_cart_api";
 
 export const API_CONFIG = {
   baseURL: API_BASE_URL,
